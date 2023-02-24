73ac1a4f0a27d7e27405b78cb162e21b5be02b29
Rahmad Noor Ikhsan <rajaikhsan2021@gmail.com>
2023-02-24 07:22:09 +0700

    My Learn Kotlin

--- src/main/kotlin/Training/Import and Package.kt
+++
@@ -1,9 +0,0 @@
-import kotlin.math.PI
-import kotlin.math.cos as cosinus
-import kotlin.math.tan as tantinus
-
-fun main() {
-    println(PI)
-    println(cosinus(10.0))
-    println(tantinus(0.1))
-}
